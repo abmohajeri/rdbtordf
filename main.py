6d5ea32eab2b76ce86a23e5aac4eeafcc4babace
Abolfazl Mohajeri <abmohajeri1@gmail.com>
2021-05-07 08:50:01 +0430

    BaseURI Added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 database = "rdf"
 username = "root"
 output = "db.rdf"
+baseURI = "http://wtlab.um.ac.ir/linkdata/"
 
-command = "dump-rdf -u " + username + " -o " + output + " jdbc:mysql:///" + database
+command = "dump-rdf -b " + baseURI + " -o " + output + " -u " + username + " jdbc:mysql:///" + database
 out = os.system(command)
